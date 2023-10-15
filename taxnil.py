55d43a8d59759051a1fda7060af7da2dbb6c7c60
SANDUNIN99 <saduni.nn@gmail.com>
2023-10-15 20:21:07 +0530

    added simple changes to the python codes

--- taxnil.py
+++ taxnil.py
@@ -6,8 +6,8 @@ def calculate_tax number(income, tax_rate):
     return tax
 
 try:
-    income = float(input("Enter your income: $"))
-    tax_rate = float(input("Enter the tax rate (as a decimal): "))
+    income = float(input("Please enter your income: $"))
+    tax_rate = float(input("Please enter the tax rate (as a decimal): "))
 
     tax_amount = calculate_tax number(income, tax_rate)
     print("Tax amount: $", tax_amount)
